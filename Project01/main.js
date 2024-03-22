144276dad6e5a07e814837884747ee755fba53f2
Muhammed Mohsin <muhammedmohsin36.9@gmail.com>
2024-03-22 23:51:15 +0500

    Calculator using typescript,inquirer

---
+++ Project01/main.js
@@ -0,0 +1,32 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter First Number", type: "number", name: "num1" },
+    { message: "Enter Second Number", type: "number", name: "num2" },
+    {
+        message: "Select one of the operator to perform action",
+        type: "list",
+        name: "operater",
+        choices: ["+", "-", "*", "/", "**", "%"],
+    },
+]);
+if (answer.operater === "+") {
+    console.log(answer.num1 + answer.num2);
+}
+else if (answer.operater === "-") {
+    console.log(answer.num1 - answer.num2);
+}
+else if (answer.operater === "*") {
+    console.log(answer.num1 * answer.num2);
+}
+else if (answer.operater === "/") {
+    console.log(answer.num1 / answer.num2);
+}
+else if (answer.operater === "**") {
+    console.log(answer.num1 ** answer.num2);
+}
+else if (answer.operater === "%") {
+    console.log(answer.num1 % answer.num2);
+}
+else {
+    console.log("Invalid Operator");
+}
